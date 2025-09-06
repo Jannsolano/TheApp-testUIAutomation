163830345d7138cfd089b16660889fde5cc03a44
Jannsolano <jann.solano@gmail.com>
2025-09-05 22:04:20 -0300

    new test cases added

--- test/specs/loginFeature.e2e.js
+++ test/specs/loginFeature.e2e.js
@@ -17,5 +17,35 @@ describe('login application', () => {
     })
     it('should try to login without user or password', async () => {
         await $('~loginBtn').click()
+        await $('//*[@text="OK"]').click()
+        await $('~Navigate Up').click()
+        await $('~Navigate Up').click()
+        await $('~Navigate Up').click()
+    })
+})
+
+describe('Usability features', () => {
+    it('Picker wheels', async () => {
+        await $('~Picker Demo').click()
+        await $('~monthPicker').click()
+        await $('//*[@text="March"]').click()
+        await $('~dayPicker').click()
+        await $('//*[@text="4"]').click()
+        await $('~learnMore').click()
+        await $('//*[@text="OK"]').click()
+        await browser.pause(2000)
+        await $('~Navigate Up').click()
+    })
+    it('Use webview to open appiumPro', async () => {
+        await $('~Webview Demo').click()
+        await $('~urlInput').setValue('https://appiumpro.com')
+        await $('~navigateBtn').click()
+        await $('~Navigate Up').click()
+    })
+    it('Use webview to open other website', async () => {
+        await $('~Webview Demo').click()
+        await $('~urlInput').setValue('https://google.com')
+        await $('~navigateBtn').click()
+        await $('//*[@text="OK"]').click()
     })
 })
